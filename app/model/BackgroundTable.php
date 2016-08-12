8291030d3158ff267c8cc905f9ab679fc2ba9414
Vladimir Litovkin <vladimir.litovkin@e-solutions.cz>
2016-08-12 04:26:08 +0200

    createTable works with switch TODO regroup l8r (etc dont duplicate <table> in every case) Main structure's been modified Foundation grid brought back

--- app/model/BackgroundTable.php
+++ app/model/BackgroundTable.php
@@ -30,17 +30,38 @@ private static $colors = array("red", "black", "blue");
 	 * @param string $type
 	 * @return string
 	 */
-	public static function createTable($type = "empty") {
-		$year = date("y"); //TODO replace with value which makes sense
-		$ret = "<table id='backgroundTable'>";
-		for ($i = 1; $i <= 12; $i++) {
-			$ret .= "<tr>";
-			for ($j = 1; $j <= cal_days_in_month(CAL_GREGORIAN, $i, $year); $j++) {
-				$ret .= "<td class='".self::$colors[rand(0, 2)]."-day'>&nbsp</td>";
-			}
-			$ret .= "</tr>";
+	public static function createTable($type = "empty", $data = null) {
+		switch ($type) {
+			case "empty":
+				$year = date("y"); //TODO replace with value which makes sense
+				$ret = "<table id='backgroundTable'>";
+				for ($i = 1; $i <= 12; $i++) {
+					$ret .= "<tr>";
+					for ($j = 1; $j <= cal_days_in_month(CAL_GREGORIAN, $i, $year); $j++) {
+						$ret .= "<td class='".self::$colors[rand(0, 2)]."-day'>&nbsp</td>";
+					}
+					$ret .= "</tr>";
+				}
+				$ret .= "</table>";
+				break;
+			case "previousYears":
+				//TODO think up some fancy ass validation for this kind of stuff
+				if (is_numeric($data) || (is_array($data) && isset($data["year"]))) {
+					$ret = "<table id='previousYearsTable'><tr>";
+					for ($i = $data; $i < 0+date("Y"); $i++) {
+						$ret .= "<td>$i</td>";
+					}
+					$ret .= "</tr></table>";
+				}
+				else {
+					throw new Nette\InvalidArgumentException("Data[...] should contain Birth year");
+				}
+				break;
+			case "":
+				break;
+			default:
+				break;
 		}
-		$ret .= "</table>";
 		return $ret;
 	}
 
