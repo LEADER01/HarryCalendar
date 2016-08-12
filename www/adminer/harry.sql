f2c3aa7abc83781c7c2039c7e11435e1146db60e
Vladimir Litovkin <vladimir.litovkin@e-solutions.cz>
2016-08-12 02:33:05 +0200

    Create new database (probably do something like "Create if not exists" l8r)

--- www/adminer/harry.sql
+++ www/adminer/harry.sql
@@ -5,6 +5,10 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP DATABASE IF EXISTS `harry`;
+CREATE DATABASE `harry` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `harry`;
+
 DROP TABLE IF EXISTS `daterating`;
 CREATE TABLE `daterating` (
   `idDateRating` int(11) NOT NULL AUTO_INCREMENT,
