ad06213ebbf129f879442d50f1a808ceb982bc4c
Vladimir Litovkin <vlitovkin@gmail.com>
2016-11-20 04:37:55 +0100

    Background table (main table) changes after logging new user

--- app/model/BackgroundTable.php
+++ app/model/BackgroundTable.php
@@ -17,20 +17,15 @@ class BackgroundTable extends SexyAssObject
 //		COLUMN_PASSWORD_HASH = 'pass',
 //		COLUMN_ROLE = 'role';
 
-private static $colors = array("red", "black", "blue");
-	/** @var Nette\Database\Context */
-	private $database; //I'm still thinking whether I want third option. TODO Settings 2v3 options?
-
-	public function __construct(Nette\Database\Context $database)
-	{
-		$this->database = $database;
-	}
+	private $colors = array("red", "black", "blue", "grey");
+	private $today = array();
 
 	/**
 	 * @param string $type
 	 * @return string
 	 */
-	public static function createTable($type = "empty", $data = null) {
+	public function createTable($type = "empty", $data = null)
+	{
 		switch ($type) {
 			case "currentYear":
 				$year = date("y"); //TODO replace with value which makes sense
@@ -40,7 +35,7 @@ private static $colors = array("red", "black", "blue");
 					$ret .= "<tr>";
 					$todaysMonth = (isset($today)) ? ($i == $today["m"]) ? true : null : null;
 					for ($j = 1; $j <= cal_days_in_month(CAL_GREGORIAN, $i, $year); $j++) {
-						$color = (isset($data["random"])) ? self::$colors[rand(0, 2)] : self::$colors[1];
+						$color = ($data["random"]) ? $this->colors[rand(0, 2)] : $this->getDayColor($j, $i, $year);
 						$ret .= "<td class='".((isset($todaysMonth)) ? ($j == $today["d"]) ? "today-day " : null : null).$color."-day c-allign'>$j</td>";
 					}
 					$ret .= "</tr>";
@@ -65,6 +60,35 @@ private static $colors = array("red", "black", "blue");
 				break;
 		}
 		return $ret;
+	}
+
+	private function getDay($d, $m, $y)
+	{
+		$day = $this->database->table("day")->where(array(
+			"day" => $d,
+			"month" => $m,
+			"year" => $y,
+		));
+		return ($day->count()) ? $day->fetch() : false;
+	}
+
+	private function getDayColor($d, $m, $y)
+	{
+		$day = $this->getDay($d, $m, $y); //TODO get day through $this and make if blow with this return
+		if ($day) {
+			$uDay = $this->database->table("userdaterating")->where(array(
+				"idUser" => $this->user->id,
+				"idDay" => $day->id
+			));
+			if ($uDay->count()) {
+				return $this->database->table("daterating")->get($uDay->idDateRating)->color;
+			} else return $this->colors[3];
+		} else return $this->colors[3];
+	}
+
+	private static function isDayStored()
+	{
+
 	}
 
 }
